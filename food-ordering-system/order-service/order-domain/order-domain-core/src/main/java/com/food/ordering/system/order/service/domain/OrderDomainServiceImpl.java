c32d5ee9bcc1590f9873b2971efa90fc4767117e
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2025-07-04 19:27:46 -0300

    add: OrderDomainServiceImpl

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -5,12 +5,17 @@ import com.food.ordering.system.order.service.domain.entity.Restaurant;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+@Slf4j
 public class OrderDomainServiceImpl implements OrderDomainService {
+
+    public static final String UTC = "UTC";
 
     @Override
     public OrderCreatedEvent validateAndInitializeOrder(Order order, Restaurant restaurant) {
@@ -18,32 +23,54 @@ public class OrderDomainServiceImpl implements OrderDomainService {
         setOrderProductInformation(order,restaurant);
         order.validateOrder();
         order.initializeOrder();
-        return  new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of("UTC")));
+        log.info("Order with Id: {} has been initialized", order.getId());
+        return  new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
     }
 
     private void setOrderProductInformation(Order order, Restaurant restaurant) {
+
+        order.getItems().forEach(orderItem -> restaurant.getProducts()
+                .forEach(restaurantProduct -> {
+                    var currentProduct = orderItem.getProduct();
+                    if (currentProduct.equals(restaurantProduct)) {
+                        currentProduct.updateWithConfirmedNameAndPrice(
+                                restaurantProduct.getName(),
+                                restaurantProduct.getPrice()
+                        );
+                    }
+                }));
     }
 
     private void validateRestaurant(Restaurant restaurant) {
+        if (!restaurant.isActive()) {
+            throw new OrderDomainException("Restaurant with id " + restaurant.getId() + " is curerntly no active");
+
+        }
     }
 
     @Override
     public OrderPaidEvent payOrder(Order order) {
-        return null;
+        order.pay();
+        log.info("Order with Id: {} has been payed", order.getId());
+        return new OrderPaidEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
     }
 
     @Override
     public void approveOrder(Order order) {
-
+        order.approve();
+        log.info("Order with Id: {} has been approved", order.getId());
     }
 
     @Override
     public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
-        return null;
+        order.initCancel(failureMessages);
+        log.info("Order Payment is ccancelling for order with Id: {} ", order.getId());
+        return new OrderCancelledEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
     }
 
     @Override
     public void CancelOrder(Order order, List<String> failureMessages) {
-
+        order.cancel(failureMessages);
+        log.info("Order with Id: {} has been cancelled", order.getId());
     }
 }
--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -15,4 +15,9 @@ public class Product extends BaseEntity<ProductId> {
     public Money getPrice() {
         return price;
     }
+
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
 }
